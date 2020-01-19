933cfa06f5bac711775171f8a7947dfed92ef199
felip <felipe.costa@zappts.com>
2020-01-19 09:33:05 -0300

    src/store/index.js - Configuração e inclusao de plugin: VuexPersistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPesistence from 'vuex-persist';
+
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
 
+
+const debug  = process.env.NODE_ENV != 'production';
+
+const VuexLocal = new VuexPesistence({  
+  storage: window.localStorage,
+  
+});
 export default new Vuex.Store({
   state: {
   },
